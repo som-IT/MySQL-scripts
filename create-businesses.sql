6f35d8f594a621d384ddf0d9280322fa3242bb46
som-IT <81415470+som-IT@users.noreply.github.com>
2024-01-05 17:20:14 +0530

    Add files via upload

---
+++ create-businesses.sql
@@ -0,0 +1,5 @@
+CREATE TABLE businesses (
+    business_name VARCHAR(200),
+    yearly_revenue INT,
+    current_status ENUM('invested', 'not-invested')
+);
---
+++ insert-businesses-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO businesses (yearly_revenue, business_name, current_status) 
+VALUES(3000, "abc commerce", "not-invested");
+
+INSERT INTO businesses (yearly_revenue, business_name, current_status) 
+VALUES(3000, "XYZ commerce", "invested");
