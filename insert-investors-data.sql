bae1c4f59c2cc783928bd307c0aea39d756370c8
som-IT <81415470+som-IT@users.noreply.github.com>
2024-01-20 20:13:49 +0530

    Add files via upload

---
+++ insert-investors-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO investors (company_name, company_address, yearly_revenue, isInvesting) VALUES("High level Inc", "hdd location", 153400, TRUE);
+
+INSERT INTO investors (company_name, company_address, yearly_revenue) VALUES("Super tech ltd", "uii location", 453400);
