95f0c637a5066b861fbb0d7b31fde80a5111619e
som-IT <81415470+som-IT@users.noreply.github.com>
2024-04-30 14:22:19 +0530

    Delete create-flights-table.sql

--- create-flights-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE flights (
-    flightNumber VARCHAR(10) PRIMARY KEY,
-    departureAirport VARCHAR(50),
-    destinationAirport VARCHAR(50),
-    departureTime TIME,
-    arrivalTime TIME,
-    flightDate DATE,
-    ticketPrice DECIMAL(10, 2)
-);
